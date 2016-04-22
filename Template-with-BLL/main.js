8513783130aa910f60bcd6ef0b2ffb598e38335c
Lizzie Prader <lprader@marvell.com>
2016-04-22 11:10:00 -0700

    require pins

--- Template-with-BLL/main.js
+++ Template-with-BLL/main.js
@@ -1,10 +1,18 @@
 //@program
+var Pins = require("pins");
 
 application.behavior = Behavior({
 	onLaunch: function(application) {
 		// Configure the BLLs used by this application
-		Pins.configure({            sensorName: {                require: "bll",
-                // Specify the pins required by this BLL                pins: {                    sensor: { pin: 61 },               	}            }		}, success => {
+		Pins.configure({
+            sensorName: {
+                require: "bll",
+                // Specify the pins required by this BLL
+                pins: {
+                    sensor: { pin: 61 },
+               	}
+            }
+		}, success => {
 			if (success) {
 		    	// After the BLLs have been configured, build the UI and issue single/repeated commands to the BLL
 		    	application.skin = new Skin({fill: "black"});
